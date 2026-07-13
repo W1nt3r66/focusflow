95e207c48bce7dafd7aaf449c5449e3e23a14132
W1nt3r66 <janasounava007@gmail.com>
2026-07-13 18:05:03 +0530

    version number

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -6,7 +6,8 @@ function AppFooter() {
       <p>
         <span className="footer-author">Sounava</span>
         <span className="footer-message"> was focused while making </span>
-        <strong className="footer-app-name">FocusFlow</strong>.
+        <strong className="footer-app-name">FocusFlow</strong>
+        <h5>v1.0.0</h5>
       </p>
     </footer>
   );
